95cabed044bdc4c5b4249b109fdaaf7b1100d2b2
Luciana Santana <luciana.santana@objective.com.br>
2022-07-03 09:50:23 -0300

    estrutura inicial

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
+
+@Module({
+  imports: [TweetsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
+
+//módulo raiz, todos os métodos são plugados aqui
